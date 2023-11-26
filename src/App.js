74683a3e880c85f6c703cd2252d700597a0f2137
Jenesyx <a.bidkhori2004@gmail.com>
2023-11-26 14:30:37 +0100

    0.1.0 - Added login panel without functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login/Login'
+import HomePage from './Pages/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route path="/" element={<HomePage/>}/> */}
-          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Login.css'
 
 function Login() {
   return (
     <>
-      
+      <div className='login-holder'>
+        <div className='login'>Login</div>
+        <div className="welcome">
+          <p>Welcome to Pro Edupage!</p>
+          <p>Please sign in to your account and start the adventure</p>
+        </div>
+        <div className="inputs">
+          <div className="username">
+            <p>Username or Lastname</p>
+            <input type="text" placeholder='Bidkhori'/>
+          </div>
+          <div className="password">
+            <p>Password</p>
+            <input type="password"/>
+          </div>
+          <div className="button">
+            <p>Sign in</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
